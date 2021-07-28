b8e374bdfa2c14173aba2f900057762e59ab1c4e
grotius-cnc <44880102+grotius-cnc@users.noreply.github.com>
2021-07-28 16:05:52 -0400

    Add files via upload

---
+++ cpp_interface/ruckig/build/CMakeFiles/ruckig.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ruckig.dir/src/brake.cpp.o"
+  "CMakeFiles/ruckig.dir/src/position-step1.cpp.o"
+  "CMakeFiles/ruckig.dir/src/position-step2.cpp.o"
+  "CMakeFiles/ruckig.dir/src/velocity-step1.cpp.o"
+  "CMakeFiles/ruckig.dir/src/velocity-step2.cpp.o"
+  "libruckig.pdb"
+  "libruckig.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ruckig.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp_interface/ruckig/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/Linux-Embedded/linux-hal/halcompile-trajectory-ruckig/ruckig
+# Build directory: /home/user/Desktop/Linux-Embedded/linux-hal/halcompile-trajectory-ruckig/ruckig/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(otg-test "/home/user/Desktop/Linux-Embedded/linux-hal/halcompile-trajectory-ruckig/ruckig/build/otg-test")
+set_tests_properties(otg-test PROPERTIES  _BACKTRACE_TRIPLES "/home/user/Desktop/Linux-Embedded/linux-hal/halcompile-trajectory-ruckig/ruckig/CMakeLists.txt;137;add_test;/home/user/Desktop/Linux-Embedded/linux-hal/halcompile-trajectory-ruckig/ruckig/CMakeLists.txt;0;")
